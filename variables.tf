929b661f5391b1644780170a643097cd30fee091
James Harmison <jharmison@redhat.com>
2021-12-23 14:58:23 -0500

    Standardize spacing

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami_type" {
-  type = string
+  type        = string
   description = "The AMI type to use, Access2 or Hourly. Availability may depend on your AWS account being linked with Red Hat Cloud Access"
-  default = "Hourly"
+  default     = "Hourly"
 
   validation {
     condition     = contains(["Hourly", "Access2"], var.ami_type)
@@ -10,9 +10,9 @@ variable "ami_type" {
 }
 
 variable "rhel_version" {
-  type = string
+  type        = string
   description = "The major version of RHEL to use for the AMI selection"
-  default = "8"
+  default     = "8"
 
   validation {
     condition     = contains(["8"], var.rhel_version)
@@ -21,28 +21,28 @@ variable "rhel_version" {
 }
 
 variable "small_flavor" {
-  type = string
+  type        = string
   description = "The AWS flavor to use for smaller instances (proxy, bastion)"
-  default = "t3.small"
+  default     = "t3.small"
 }
 
 variable "large_flavor" {
-  type = string
+  type        = string
   description = "The AWS flavor to use for the larger instance (registry)"
-  default = "t3.large"
+  default     = "t3.large"
 }
 
 variable "public_key" {
-  type = string
+  type        = string
   description = "The SSH public key string to use for the instances"
 }
 
 variable "cluster_name" {
-  type = string
+  type        = string
   description = "The name you will be giving your OpenShift cluster in metadata.name in install-config.yaml (Note that all resources created are scoped under this subdomain)"
 }
 
 variable "cluster_domain" {
-  type = string
+  type        = string
   description = "The name of the domain under which your OpenShift cluster will reside (Note that this needs to be a Hosted Zone managed in Route53)"
 }
