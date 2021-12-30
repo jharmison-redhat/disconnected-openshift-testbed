4cbe1851179bd16af8970c135574e5ae1f8d352b
James Harmison <jharmison@redhat.com>
2021-12-30 15:25:10 -0500

    Begin manual testing of quay ansible role

--- modules/quay_registry/quay.sh
+++ modules/quay_registry/quay.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 sudo dnf -y update
+sudo dnf -y install '@container-tools' python36-pip
 
-# Ansible stuff here
+pip3 install --user upgrade pip setuptools wheel
+
+cat << REQUIREMENTS > requirements.txt
+jmespath
+cryptography
+ansible
+ansible-galaxy
+REQUIREMENTS
+
+pip3 install --user -r requirements.txt
+
+cat << REQUIREMENTS > requirements.yml
+---
+collections:
+- ansible.posix
+- containers.podman
+- community.crypto
+roles:
+- jharmison_redhat.redhat_quay
+REQUIREMENTS
+
+ansible-galaxy install -r requirements.yml
 
 sudo reboot now
