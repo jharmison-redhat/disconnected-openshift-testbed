60160649060f7fc9b48e7cfccf957445ccc28377
James Harmison <jharmison@redhat.com>
2021-12-30 16:17:30 -0500

    A little bit of whitespace

--- modules/disconnected_vpc/outputs.tf
+++ modules/disconnected_vpc/outputs.tf
@@ -2,6 +2,7 @@ output "private_subnets" {
   value       = [for subnet in aws_subnet.private : subnet.id]
   description = "The IDs of the subnets that route through the proxy."
 }
+
 output "public_subnets" {
   value       = [for subnet in aws_subnet.public : subnet.id]
   description = "The IDs of the subnets that route through the IGW."
