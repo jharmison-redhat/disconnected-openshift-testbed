f9dee3260019b13669f1890bcac4389ab359f923
James Harmison <jharmison@redhat.com>
2021-12-31 10:06:00 -0500

    Made simple example actually simple

--- examples/simple-usage/main.tf
+++ examples/simple-usage/main.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
-  }
-}
-
-resource "random_string" "name_suffix" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-resource "random_password" "instance_password" {
-  length  = 16
-  special = false
-}
-
 module "testbed" {
   # When using this module, you should use the following commented out URL:
   # source = "github.com/jharmison-redhat/disconnected-openshift-testbed"
@@ -26,25 +6,16 @@ module "testbed" {
   # to indicate that you're using the working copy in the parent directory:
   source = "../.."
 
-  # This defaults to hourly, but using an AWS account with Cloud Access set up
-  # means we can take advantage of cheaper instances like this:
-  ami_type = "Access2"
   # This is required to be provided
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5Da2XARZmB8KsjASv6MQoAS6sAXrw0yE5Y8ANJ5yTG"
-  # These are required and here are generated semi-randomly to prevent naming
-  # collisions
-  cluster_name      = "${var.cluster_name}_${random_string.name_suffix.result}"
-  cluster_domain    = var.cluster_domain
-  instance_password = random_password.instance_password.result
+  public_key     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5Da2XARZmB8KsjASv6MQoAS6sAXrw0yE5Y8ANJ5yTG"
+  cluster_name   = var.cluster_name
+  cluster_domain = var.cluster_domain
 }
 
 provider "aws" {
   region = "us-west-2"
   default_tags {
     tags = {
-      # I would like these to have obvious names, but alas....
-      # https://github.com/hashicorp/terraform-provider-aws/issues/19583
-      #      Name    = "${var.cluster_name}_${random_string.name_suffix.result}.${var.cluster_domain}"
       Name    = "${var.cluster_name}.${var.cluster_domain}"
       Project = "disconnected-openshift-testbed"
     }
--- examples/simple-usage/outputs.tf
+++ examples/simple-usage/outputs.tf
@@ -2,9 +2,3 @@ output "registry_hostname" {
   value       = module.testbed.registry_url
   description = "The public DNS hostname for the Quay registry."
 }
-
-output "instance_password" {
-  value       = random_password.instance_password.result
-  description = "The randomly generated password used for instances."
-  sensitive   = true
-}
