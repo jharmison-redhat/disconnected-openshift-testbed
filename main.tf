de64b7235c55eabf419d8c0b15308c5c864b4103
James Harmison <jharmison@redhat.com>
2022-01-10 14:37:03 -0500

    Cleanup (#4)
    - Removed complex Ansible example for refactoring elsewhere
    - Renamed some things lingering from previous setups
    - Improved documentation

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ terraform {
   }
 }
 
+# The AMI selected for instance creation is the latest official image
 data "aws_ami" "rhel" {
   most_recent = true
   owners      = ["309956199498"]
@@ -25,21 +26,25 @@ data "aws_ami" "rhel" {
 
 data "aws_region" "current" {}
 
+# Pull all availability zones that are available in this region
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# Identify the Hosted Zone that matches the provided domain name
 data "aws_route53_zone" "public" {
   name = var.cluster_domain
 }
 
+# Keypair that is created using the public key provided in vars
 resource "aws_key_pair" "ec2_key" {
   key_name   = "${var.cluster_name}_${replace(var.cluster_domain, ".", "_")}"
   public_key = var.public_key
 }
 
+# This submodule instantiates the VPC, subnets, and the bastion and proxy instances
 module "vpc" {
-  source             = "./modules/disconnected_vpc"
+  source             = "./modules/vpc"
   availability_zones = slice(data.aws_availability_zones.available.names, 0, 3)
   ami_id             = data.aws_ami.rhel.id
   proxy_flavor       = var.small_flavor
@@ -50,8 +55,9 @@ module "vpc" {
   hosted_zone        = data.aws_route53_zone.public.id
 }
 
+# This submodule creates the instance for the registry, as well as the S3 bucket for registry content
 module "registry" {
-  source            = "./modules/quay_registry"
+  source            = "./modules/registry"
   availability_zone = data.aws_availability_zones.available.names[0]
   ami_id            = data.aws_ami.rhel.id
   flavor            = var.large_flavor
