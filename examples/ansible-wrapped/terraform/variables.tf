0b10a251b55a86d6d1ad2a4e7ae0d0a1bbb051b3
James Harmison <jharmison@redhat.com>
2022-01-10 08:46:32 -0500

    Stripped ansible example out to go to another repo

--- examples/ansible-wrapped/terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "cluster_name" { default = "disco" }
-variable "cluster_domain" { default = "redhat4govaws.io" }
-variable "public_key" { default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5Da2XARZmB8KsjASv6MQoAS6sAXrw0yE5Y8ANJ5yTG" }
-variable "region" { default = "us-west-2" }
