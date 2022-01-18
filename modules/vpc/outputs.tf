1110b971bc758d31cab27ef9cd8e87f902af69a6
James Harmison <jharmison@redhat.com>
2022-01-18 17:10:17 -0500

    Added more detail to output on subnets

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,11 @@
 output "private_subnets" {
-  value       = [for subnet in aws_subnet.private : subnet.id]
-  description = "The IDs of the subnets that route through the proxy."
+  value       = aws_subnet.private
+  description = "Details about the subnets that are isolated by routing through the proxy."
 }
 
 output "public_subnets" {
-  value       = [for subnet in aws_subnet.public : subnet.id]
-  description = "The IDs of the subnets that route through the IGW."
+  value       = aws_subnet.public
+  description = "Details about the subnets that route through the IGW to the public internet."
 }
 
 output "proxy_instance" {
